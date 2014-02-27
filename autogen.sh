f1ec80c9cee2e9c948317fc09de31cb46c155248
Javier Jardón <jjardon@gnome.org>
2014-02-27 02:14:06 +0000

    autogen.sh:  Use autoreconf instead a custom script

--- autogen.sh
+++ autogen.sh
@@ -1,55 +1,20 @@
-builddir=`pwd`
-srcdir=`dirname $0`
+#!/bin/sh
+# Run this to generate all the initial makefiles, etc.
+
+test -n "$srcdir" || srcdir=`dirname "$0"`
+test -n "$srcdir" || srcdir=.
+
+OLDDIR=`pwd`
 cd $srcdir
 
-mkdir -p .auto
-
-echo "Running glib-gettextize ..."
-glib-gettextize --force --copy || {
-  echo "**Error**: glib-gettextize failed."; exit 1; }
-
-# echo "Running intltoolize ..."
-# intltoolize --force --copy --automake ||
-# 	{ echo "**Error**: intltoolize failed."; exit 1; }
-
-libtoolize --force --copy || {
-  echo "**ERROR**: libtoolize failed."; exit 1; }
-
-echo "Running aclocal $ACLOCAL_FLAGS ..."
-aclocal $ACLOCAL_FLAGS || {
-  echo
-  echo "**Error**: aclocal failed. This may mean that you have not"
-  echo "installed all of the packages you need, or you may need to"
-  echo "set ACLOCAL_FLAGS to include \"-I \$prefix/share/aclocal\""
-  echo "for the prefix where you installed the packages whose"
-  echo "macros were not found"
-  exit 1
-}
-
-echo "Running autoconf ..."
-WANT_AUTOCONF=2.5 autoconf || {
-  echo "**Error**: autoconf failed."; exit 1; }
-
-echo "Running autoheader..."
-WANT_AUTOCONF=2.5 autoheader --force || {
-  echo "**Error**: autoheader failed."; exit 1; }
-
-# checking for automake 1.9+
-am_version=`automake --version | head -n 1 | cut -f2- -d1 | cut -f2 -d.`
-[ "${am_version}" -ge 9 ] || {
-  echo "**Error**: automake 1.9+ required."; exit 1; }
-
-echo "Running automake --gnu $am_opt ..."
-automake --add-missing --gnu $am_opt || {
-  echo "**Error**: automake failed."; exit 1; }
-
-conf_flags="--enable-maintainer-mode --enable-compile-warnings"
-
-if test x$NOCONFIGURE = x; then
-  echo Running $srcdir/configure $conf_flags "$@" ...
-  cd $builddir
-  $srcdir/configure $conf_flags "$@" \
-  && echo Now type \`make\' to compile $PKG_NAME || exit 1
-else
-  echo Skipping configure process.
+AUTORECONF=`which autoreconf`
+if test -z $AUTORECONF; then
+        echo "*** No autoreconf found, please intall it ***"
+        exit 1
 fi
+
+autoreconf --force --install --verbose || exit $?
+
+cd $OLDDIR
+test -n "$NOCONFIGURE" || "$srcdir/configure" "$@"
+
